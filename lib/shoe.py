3bd27d53244db4626d570feba455642bdbc0fee1
joseph <michealkinuthia.m.k@gmail.com>
2024-03-25 11:51:23 +0300

    .

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    class Shoe:
+    def __init__(self, size, style, color, brand, price):
+        self.size = size
+        self.style = style
+        self.color = color
+        self.brand = brand
+        self.price = price
+
+    def calculate_cost(self, tax_percentage):
+        total_cost = self.price * (1 + tax_percentage)
+        return total_cost
+
+    def add_to_cart(self, cart):
+        cart.append(self)
